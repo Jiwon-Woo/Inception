5bf38cb5eaada88f008f98b2542ad6cff686eac9
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-08-11 01:21:35 +0900

    REFACT: wordpress pid 1로 변경

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-cat << EOF > exit.sql
-exit
-EOF
-
-while ! mysql -hmariadb -P3306 -ujwoo -pjwoo42 < exit.sql ; do
-	if [ $status -ge 30 ]; then
-		printf "FAILED connect to db\n"
-		exit 1
-	fi
-	printf "Trying to connect to db ($status/30)\n"
-	status=$((status+1))
-	sleep 1
-done
-echo -e "Success."
-
 /etc/init.d/php7.3-fpm start
 /etc/init.d/php7.3-fpm status
+
+# RUN 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+# RUN 	chmod +x /tmp/wordpress.sh \
+# 		&& chmod +x wp-cli.phar \
+# 		&& mv wp-cli.phar /usr/local/bin/wp \
+# 		&& wp core download --version=5.8.1 --path=/var/www/wordpress
+
 /etc/init.d/php7.3-fpm stop
-/usr/sbin/php-fpm7.3 -F
+
+exec /usr/sbin/php-fpm7.3 -F
